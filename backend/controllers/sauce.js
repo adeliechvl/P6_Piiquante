4f8fb9d0af7d16242d24e1ca002b6e901ba72781
Adélie Chauvel <adelie.chauvel@yahoo.fr>
2023-01-23 22:50:21 +0100

    Ajout de sauces

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -21,26 +21,20 @@ exports.createSauce = (req, res, next) => {
 };
 
 // MODIFIER UNE SAUCE
-exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file ? {
-    ...JSON.parse(req.body.sauce),
-    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-  } : { ...req.body };
-
-  delete sauceObject._userId;
-  Sauce.findOne({ _id: req.params.id })
-    .then((sauce) => {
-      if (sauce.userId != req.auth.userId) {
-        res.status(401).json({ message: 'Not authorized' });
-      } else {
-        sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Sauce modifiée!' }))
-          .catch(error => res.status(401).json({ error }));
-      }
-    })
-    .catch((error) => {
-      res.status(400).json({ error });
-    });
+exports.modifyThing = (req, res, next) => {
+  Sauce.findOne({_id: req.params.id})
+      .then((thing) => {
+          if (thing.userId != req.auth.userId) {
+              res.status(401).json({ message : 'Not authorized'});
+          } else {
+              Sauce.updateOne({ _id: req.params.id}, { ...sauceObject, _id: req.params.id})
+              .then(() => res.status(200).json({message : 'Sauce modifiée!'}))
+              .catch(error => res.status(401).json({ error }));
+          }
+      })
+      .catch((error) => {
+          res.status(400).json({ error });
+      });
 };
 
 // SUPPRIMER UNE SAUCE
