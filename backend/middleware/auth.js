f7a0564c24e64f409396eabf325b777891c48f1b
Adélie Chauvel <adelie.chauvel@yahoo.fr>
2023-02-08 19:33:41 +0100

    process.env.token

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
       const token = req.headers.authorization.split(' ')[1];
-      const decodedToken = jwt.verify(token,'mongodb');
+      const decodedToken = jwt.verify(token, process.env.token);
       const userId = decodedToken.userId;
       req.auth = { userId };
       if (req.body.userId && req.body.userId !== userId) {
